3ec6328c0bdb5e274271ec37c4ba8fa1326abbc6
oolaoluwatobi <58662625+oolaoluwatobi@users.noreply.github.com>
2023-08-31 21:23:33 +0100

    cors update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'http://localhost:3500',
   'http://localhost:5173',
   'https://kanban-client-sigma.vercel.app/',
+  'https://kanban-client-sigma.vercel.app/',
   'https://sage-starship-ebf074.netlify.app/'
 ];
 
